396950ae8a262ab46aadf932185fb71b0f7e3e24
om patel <134964209+Ghosst141@users.noreply.github.com>
2024-04-14 02:42:34 +0530

    first commit

---
+++ To-Do-List/index.js
@@ -0,0 +1,40 @@
+const btn = document.getElementById('btn');
+const textfield = document.getElementById('textfield');
+const ul = document.getElementById('todo');
+
+btn.addEventListener('click', addtodo);
+
+function addtodo() {
+    const txt = document.createTextNode(textfield.value);
+    const li = document.createElement('li');
+    const delbtn = document.createElement('button');
+    const datediv=document.createElement('div');
+    const textdiv=document.createElement('div');
+
+
+    let date= new Date();
+    date=date.toLocaleDateString();
+    date=document.createTextNode(date);
+    delbtn.innerText='X';
+
+    delbtn.setAttribute('class','delbtn');
+    datediv.setAttribute('class','date');
+    textdiv.setAttribute('class','textdiv');
+    li.setAttribute('class','list');
+    li.style.listStyleType='none';
+
+    datediv.appendChild(date);
+    textdiv.appendChild(txt)
+    li.appendChild(datediv);
+    li.appendChild(textdiv);
+    li.appendChild(delbtn);
+
+    delbtn.addEventListener('click',()=>{
+        li.remove();
+    })
+
+    if (textfield.value) {
+        ul.appendChild(li)
+    }
+    textfield.value="";
+}
